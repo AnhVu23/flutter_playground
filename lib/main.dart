d6fb6e344d9d1c7de198588542982734e586652e
anhvu-95 <vu.haianh291@gmail.com>
2020-08-26 16:20:11 +0300

    Handle the case when running of questions using ternary operators

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import './question.dart';
 import './answer.dart';
+
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -12,40 +13,48 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
-  void _answerQuestion() {
-    setState(() {
-      _questionIndex++;
-    });
-  }
-  @override
-  Widget build(BuildContext context) {
-    var questions = [{
+  static const questions = [
+    {
       'questionText': 'What \'s your favorite animals?',
       'answer': ['Dog', 'Cat', 'Elephant']
-    }, {
+    },
+    {
       'questionText': 'What \'s your favorite sports?',
       'answer': ['Football', 'Basketball', 'Floorball']
-    }, {
+    },
+    {
       'questionText': 'What \'s your favorite programming language?',
       'answer': ['Python', 'Java', 'Dart']
     },
-    ];
+  ];
+  void _answerQuestion() {
+    if (_questionIndex < questions.length) {
+      setState(() {
+        _questionIndex++;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter playground'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter playground'),
+        ),
+        body: _questionIndex < questions.length ? Column(children: [
+          Question(questions[_questionIndex]['questionText']),
+          ...(questions[_questionIndex]['answer'] as List<String>)
+              .map((answer) {
+            return Answer(_answerQuestion, answer);
+          }).toList()
+        ]) : Center(child: Text('Running out of question')),
       ),
-      body: Column(children: [
-        Question(questions[_questionIndex]['questionText']),
-        ...(questions[_questionIndex]['answer'] as List<String>).map((answer) {
-          return Answer(_answerQuestion, answer);
-        }).toList()
-      ]),
-      
-    ), );
+    );
   }
 }
 
 void main() {
-  runApp(MyApp());    
+  runApp(MyApp());
 }
