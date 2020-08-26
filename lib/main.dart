a7d57f62d3301dadd81bc4fcebf845a81db26cfe
anhvu-95 <vu.haianh291@gmail.com>
2020-08-26 14:36:07 +0300

    Try stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  void answerQuestion() {
+    setState(() {
+      questionIndex++;
+    });
+  }
   @override
   Widget build(BuildContext context) {
+    List<String> questions = [
+      'First question',
+      'secondQuestion',
+      'third question'
+    ];
     // TODO: implement build
     return MaterialApp(home: Scaffold(
       appBar: AppBar(
         title: Text('Flutter playground'),
       ),
-      body: Text('This is my default text!'),
+      body: Column(children: [
+        Text(questions.elementAt(questionIndex)),
+        RaisedButton(child: Text('1'), onPressed: answerQuestion,),
+        RaisedButton(child: Text('2'), onPressed: answerQuestion,),
+        RaisedButton(child: Text('3'), onPressed: answerQuestion,),
+      ]),
       
     ), );
   }
-}
+  }
 
 void main() {
   runApp(MyApp());    
